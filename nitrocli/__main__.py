607d791ea2d41b1c657ae2b4ed2bf3bc4699610b
R <vonshednob-5639@kakaomilchkuh.de>
2018-12-09 11:21:08 +0100

    Generalised the menu

--- nitrocli/__main__.py
+++ nitrocli/__main__.py
@@ -1,4 +1,23 @@
-from nitrocli.menu import main
+from nitrocli.lib import NitroKey
+from nitrocli.menu import Menu
 
 
-main()
+def init(menu):
+    menu.nk = NitroKey()
+    menu.nk.do_connect()
+
+
+def cleanup(menu):
+    menu.nk.lock()
+    menu.nk.logout()
+
+
+options = []
+commands = {'quit': Menu.quit}
+
+
+Menu(options,
+     data={'nk': None},
+     init=init,
+     cleanup=cleanup,
+     commands=commands).run()
